132da4ff35ebddb2d05fdc46bfda8e964242a688
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2025-03-29 11:18:40 -0500

    Scanner for user input

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
-
+        Scanner scnr = new Scanner(System.in);
         int health = 100;
+
+
+        //Beginning of story
         
+
         
     }
 }
